8988e3c888817931654ced621052d6675a722475
Badhia <byuneskatz@flowwest.com>
2025-09-24 08:18:09 -0700

    modifies clean_data script, regenerates and validates xml

--- data-raw/clean_data_09_25.R
+++ data-raw/clean_data_09_25.R
@@ -1,25 +1,22 @@
 library(tidyverse)
 library(readxl)
-# Note that we will no longer use this clean data script.
-# Ashley met with Claire to update queries to remove the appropriate tables
-# All additional processing will happen when we pull in data to database.
 
-# TODO - check if above notes are still accurate
-# catch  #TODO checked with Claire on why visitTime2 was not added, waiting for answer
-catch_raw <- read_xlsx(here::here("data-raw", "LFR RST Catch Raw EDI Query.v.11.15.24.xlsx")) |>
-  select(-actualCount) |> # should we delete this row, as in original clean_data?
+# catch  #
+catch_raw <- read_xlsx(here::here("data-raw", "lower_feather_catch.xlsx")) |>
+  select(-actualCount) |>
   glimpse()
 
 write_csv(catch_raw, here::here("data", "lower_feather_catch.csv"))
 
-# trap - #TODO checked with Claire on why visitTime2 was not added, waiting for answer
-trap_raw <- read_xlsx(here::here("data-raw", "LFR RST Trap Visit EDI Query.v.11.15.24.xlsx")) |>
+# trap
+trap_raw <- read_xlsx(here::here("data-raw", "lower_feather_trap.xlsx")) |>
   mutate(waterTempUnit = if_else(waterTempUnit == "°C", "Celsius", "Fahrenheit")) |> # is this code not necessary?
+  select(-counterAtStart) |>
   glimpse()
 write_csv(trap_raw, here::here("data", "lower_feather_trap.csv"))
 
 # recaptures
-recaptures_raw <- read_xlsx(here::here("data-raw", "LFR RST Recapture EDI Query.v.11.15.24.xlsx")) |> # note that there are 11 na values for finalRun
+recaptures_raw <- read_xlsx(here::here("data-raw", "lower_feather_recapture.xlsx")) |> # note that there are 11 na values for finalRun
   mutate(trap_start_date = ymd_hms(case_when(visitType %in% c("Continue trapping", "Unplanned restart", "End trapping") ~ lag(visitTime2),
                                              T ~ visitTime)),
          trap_end_date = ymd_hms(case_when(visitType %in% c("Continue trapping", "Unplanned restart", "End trapping") ~ visitTime,
@@ -29,12 +26,12 @@ recaptures_raw <- read_xlsx(here::here("data-raw", "LFR RST Recapture EDI Query.
 write_csv(recaptures_raw, here::here("data", "lower_feather_recapture.csv"))
 
 # releases
-release_raw <- read_xlsx(here::here("data-raw", "LFR RST Release EDI Query.v.11.15.24.xlsx")) |>
+release_raw <- read_xlsx(here::here("data-raw", "lower_feather_release.xlsx")) |>
   glimpse()
 write_csv(release_raw, here::here("data", "lower_feather_release.csv"))
 
 # releasefish
-release_fish_raw <- read_xlsx(here::here("data-raw", "LFR RST ReleaseFish EDI Query.v.11.15.24.xlsx")) |> # note that all forklength values are empty
+release_fish_raw <- read_xlsx(here::here("data-raw", "lower_feather_releasefish.xlsx")) |> # note that all forklength values are empty
   glimpse()
 write_csv(release_fish_raw, here::here("data", "lower_feather_release_fish.csv"))
 
