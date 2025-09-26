02365064c2c8cd5109b29ae438f9745e7a21c2a8
Badhia <byuneskatz@flowwest.com>
2025-09-26 09:28:35 -0700

    adds sourceOfFishSite to release table, generates data file and updates metadata

--- data-raw/clean_data_09_25.R
+++ data-raw/clean_data_09_25.R
@@ -40,11 +40,17 @@ recaptures_raw <- read_xlsx(here::here("data-raw", "lower_feather_recapture.xlsx
 write_csv(recaptures_raw, here::here("data", "lower_feather_recapture.csv"))
 
 # releases
-release_raw <- read_xlsx(here::here("data-raw", "lower_feather_release.xlsx")) |> #no markLifeStage, sourceOfFishSite, appliedMarkCode
+release_raw <- read_xlsx(here::here("data-raw", "lower_feather_release.xlsx")) |> #no markLifeStage, appliedMarkCode
+  # asked Gabe an about sourceOfFishSite (and other missing fields):
+  #For the 2025 trapping season all fish used for Etrials were from the Feather River Fish Hatchery
+  mutate(sourceOfFishSite = case_when(year(releaseTime) == 2025 ~ "Feather River Fish Hatchery",
+                                      TRUE ~ NA)) |>
+  relocate(sourceOfFishSite, .after = markedFishOrigin) |>
   glimpse()
+
 write_csv(release_raw, here::here("data", "lower_feather_release.csv"))
 
-# releasefish
+gi# releasefish
 release_fish_raw <- read_xlsx(here::here("data-raw", "lower_feather_releasefish.xlsx")) |> # note that all forklength values are empty
   glimpse()
 write_csv(release_fish_raw, here::here("data", "lower_feather_release_fish.csv"))
