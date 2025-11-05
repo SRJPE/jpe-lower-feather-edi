7fa02b34bf361a453747ea6130bc74afed50536c
Badhia <byuneskatz@flowwest.com>
2025-11-05 15:25:58 -0800

    changes path to main branch, generates xml

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -21,7 +21,7 @@ datatable_metadata <-
                                           "Recaptured fish",
                                           "Released fish",
                                           "Released fish"), #TODO check this description
-                datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/jpe-lower-feather-edi/edi-updates-9-25/data/", #TODO updates this path when merged
+                datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/jpe-lower-feather-edi/main/data/",
                                        c("lower_feather_catch.csv",
                                          "lower_feather_trap.csv",
                                          "lower_feather_recapture.csv",
