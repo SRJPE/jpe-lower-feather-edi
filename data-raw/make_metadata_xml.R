1fa9152655b8421fc5a7ed16a0d2df0b54f817b7
Badhia <byuneskatz@flowwest.com>
2025-12-16 14:43:46 -0800

    updates folder path, writes xml

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -21,7 +21,7 @@ datatable_metadata <-
                                           "Recaptured fish",
                                           "Released fish",
                                           "Released fish"), #TODO check this description
-                datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/jpe-lower-feather-edi/updates-2025/",
+                datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/jpe-lower-feather-edi/updates-2025/data/",
                                        c("lower_feather_catch.csv",
                                          "lower_feather_trap.csv",
                                          "lower_feather_recapture.csv",
